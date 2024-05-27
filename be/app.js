1a303a7911410e7168c6c1acfee1edd5918fa23e
jebi2420 <jebi2420@gmail.com>
2024-05-27 21:26:43 +0900

    [chors] 코드 문법 실수 수정

--- be/app.js
+++ be/app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json()); // req.body가 객체로 인식된다
 
 // mongoDB 세팅
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
-mongoose.connect(mongoURI, {useNewUrlParser:true}
+mongoose.connect(mongoURI, {useNewUrlParser:true})
     .then(()=>console.log("mongoose connected"))
-    .catch((err)=>console.log("DB connection fail", error)));
+    .catch((err)=>console.log("DB connection fail", error));
 
 // port에 서버 오픈
 app.listen(process.env.PORT || 5001, ()=>{

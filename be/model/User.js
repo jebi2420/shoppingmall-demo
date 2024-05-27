c01ef5e6b31ec3c1af8ea2d2a29cfbf9f17a97ac
jebi2420 <jebi2420@gmail.com>
2024-05-27 22:07:28 +0900

    [feat] User 모델 생성

---
+++ be/model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    level: {
+        type: String,
+        default: "customer" // 2types: customer, admin(관리자)
+    }
+},{timestamps:true});
+
+userSchema.methods.toJSON = function (){
+    const obj = this._doc // this._doc = user 데이터 하나
+    delete obj.password
+    delete obj.__v
+    delete obj.updateAt
+    delete obj.createAt
+    return obj
+}
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;

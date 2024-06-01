bf02e38b763d4e471a49037c0b04d545404efda9
jebi2420 <jebi2420@gmail.com>
2024-06-01 20:06:06 +0900

    [feat] dev tool 추가

--- fe/src/reducer/store.js
+++ fe/src/reducer/store.js
@@ -1,17 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
 import userReducer from "./userReducer";
 import productReducer from "./productReducer";
 import cartReducer from "./cartReducer";
 import commonUiReducer from "./commonUIReducer";
 import orderReducer from "./orderReducer";
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    product: productReducer,
-    cart: cartReducer,
-    ui: commonUiReducer,
-    order: orderReducer,
-  },
+// rootReducer를 combineReducers를 사용하여 합침
+const rootReducer = combineReducers({
+  user: userReducer,
+  product: productReducer,
+  cart: cartReducer,
+  ui: commonUiReducer,
+  order: orderReducer,
 });
+
+// Redux DevTools를 사용하여 store를 생성
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 export default store;

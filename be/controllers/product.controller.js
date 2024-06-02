e375c4d1e924e25296400719593262ebb5986ad1
jebi2420 <jebi2420@gmail.com>
2024-06-02 17:54:46 +0900

    [fix] size 속성 삭제

--- be/controllers/product.controller.js
+++ be/controllers/product.controller.js
@@ -6,7 +6,7 @@ productController.createProduct = async (req, res) =>{
     try{
         const {sku, name, size, image, category, description, price, stock, status} = req.body;
         // 모든 필수 정보가 입력되었는지 확인
-        if (!sku || !name || !size || !image || !category || !description || !price || !stock || !status) {
+        if (!sku || !name  || !image || !category || !description || !price || !stock || !status) {
             throw new Error("상품의 정보값을 모두 입력해주세요");
         }
         // sku 중복 방지
@@ -17,7 +17,6 @@ productController.createProduct = async (req, res) =>{
         const product = new Product({
             sku, 
             name, 
-            size, 
             image, 
             category, 
             description, 

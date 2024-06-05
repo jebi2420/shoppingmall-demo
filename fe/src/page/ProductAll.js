7e3169d44148a527517801fbb41a85ef0583e3a9
jebi2420 <jebi2420@gmail.com>
2024-06-05 22:20:11 +0900

    [feat] 랜딩 페이지 전체 상품 보여주기 구현

--- fe/src/page/ProductAll.js
+++ fe/src/page/ProductAll.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../component/ProductCard";
 import { Row, Col, Container } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
@@ -9,14 +9,26 @@ import { commonUiActions } from "../action/commonUiAction";
 const ProductAll = () => {
   const dispatch = useDispatch();
   const error = useSelector((state) => state.product.error);
+  const { productList } = useSelector(state=>state.product);
+  const [query, setQuery] = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState({
+    page: query.get("page") || 1,
+    name: query.get("name") || "",
+  }); //검색 조건들을 저장하는 객체
+
   // 처음 로딩하면 상품리스트 불러오기
+  useEffect(()=>{
+    dispatch(productActions.getProductList({...searchQuery}));
+  },[query]);
 
   return (
     <Container>
       <Row>
-        <Col md={3} sm={12}>
-          <ProductCard />
-        </Col>
+        {productList.map((item, index) => (
+          <Col key={index} md={3} sm={12}>
+            <ProductCard item={item}/>
+          </Col>
+        ))}
       </Row>
     </Container>
   );

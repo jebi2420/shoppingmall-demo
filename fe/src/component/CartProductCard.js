5e2149f5cf12e1574e43dd580512fac45b475479
jebi2420 <jebi2420@gmail.com>
2024-06-09 21:08:32 +0900

    [feat] 카트 수량 재고수량만큼 변경 기능 구현
    상품의 재고가 담아둔 상품의 재고수량보다 적을때 상품재고로 카트상품수량 변경

--- fe/src/component/CartProductCard.js
+++ fe/src/component/CartProductCard.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect, useState  } from "react";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Row, Col, Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../action/cartAction";
 import { currencyFormat } from "../utils/number";
+import { commonUiActions } from '../action/commonUiAction';
 
 const CartProductCard = ({ item }) => {
   const dispatch = useDispatch();
+  let availableStock = 0;
 
   const handleQtyChange = (id, size, value) => {
     //아이템 수량을 수정한다
     dispatch(cartActions.updateQty(id, size, value));
+    // console.log("cart", item)
+    // console.log("stock",item.qty) 
+    // console.log("productAvailable", item.productId.stock)
+    // console.log("avail",  productSize, productValue)
   };
 
   const deleteCart = (id, size) => {
@@ -19,8 +25,23 @@ const CartProductCard = ({ item }) => {
     dispatch(cartActions.deleteCartItem(id, size));
   };
 
+  const getAvailableStock = (qty) => {
+    // 재고보다 담아둔 카트수량이 많으면 카트수량을 재고수량만큼 변경
+    Object.keys(item.productId.stock).map((size, index)=> {
+      if(size === item.size){
+        availableStock = item.productId.stock[size];
+      }
+    });
+    if(availableStock < qty) {
+      return availableStock;
+    }
+      return qty;
+  }
+
   return (
-    <div className="product-card-cart">
+    <div 
+      className="product-card-cart"
+    >
       <Row>
         <Col md={2} xs={12}>
           <img
@@ -45,12 +66,23 @@ const CartProductCard = ({ item }) => {
           </div>
           <div>Size: {item.size.toUpperCase()}</div>
           <div>Total: ₩ {currencyFormat(item.productId.price * item.qty)}</div>
+          <div>{Object.keys(item.productId.stock).map((size, index)=> {
+            if(size === item.size && item.productId.stock[size] <= 5 && item.productId.stock[size] > 0){
+              return(
+                <div className='stock'>재고: {item.productId.stock[size]}</div>
+              );
+            }
+            return null
+          }
+          )} 
+          
+          </div>
           <div>
             Quantity:
             <Form.Select
               onChange={(event) => handleQtyChange(item.productId._id, item.size, event.target.value)}
               required
-              defaultValue={item.qty}
+              defaultValue={getAvailableStock(item.qty)}
               className="qty-dropdown"
             >
               <option value={1}>1</option>

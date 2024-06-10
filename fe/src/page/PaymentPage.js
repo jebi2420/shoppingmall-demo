606ad97a6506447cda7411ce1e60f258a8f2a5e4
jebi2420 <jebi2420@gmail.com>
2024-06-10 13:13:42 +0900

    [feat] 오더 생성 프론트엔드

--- fe/src/page/PaymentPage.js
+++ fe/src/page/PaymentPage.js
@@ -36,7 +36,24 @@ const PaymentPage = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const { firstName, lastName, contact, address, city, zip }= shipInfo;
+
+    const data = {
+      totalPrice,
+      shipTo: { address, city, zip },
+      contact: { firstName, lastName, contact },
+      orderList: cartList.map(item=>{
+        return {
+          productId: item.productId._id,
+          price: item.productId.price,
+          qty: item.qty,
+          size: item.size
+        }
+      })
+    };
+
     //오더 생성하기
+    dispatch(orderActions.createOrder(data));
   };
 
   const handleFormChange = (event) => {

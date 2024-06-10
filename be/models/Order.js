e3224db2676ce427cacdff8792adcf810fb7ca65
jebi2420 <jebi2420@gmail.com>
2024-06-10 18:56:00 +0900

    [feat] order 생성 후 cart 비우기

--- be/models/Order.js
+++ be/models/Order.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Product = require('./Product');
 const User = require('./User');
+const Cart = require('./Cart');
 const Schema = mongoose.Schema;
 
 const orderSchema = Schema({
@@ -56,6 +57,13 @@ orderSchema.methods.toJSON = function () {
     delete obj.createAt
     return obj
 }
+
+orderSchema.post("save", async function(){
+    // 카트를 비워주자
+    const cart = await Cart.findOne({ userId: this.userId });
+    cart.items = [];
+    await cart.save();
+});
 
 const Order = mongoose.model("Order", orderSchema);
 module.exports = Order;

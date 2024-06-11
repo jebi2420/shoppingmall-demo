29c5dd0bccb75d272553f7331afb7f7fceaa1422
jebi2420 <jebi2420@gmail.com>
2024-06-11 16:16:22 +0900

    [style] admin 페이지 sidebar 스타일 수정

--- fe/src/component/Sidebar.js
+++ fe/src/component/Sidebar.js
@@ -17,7 +17,7 @@ const Sidebar = () => {
         <Link to="/">
           <img width={100} src="/image/hm-logo.png" alt="hm-logo.png" />
         </Link>
-        <div className="sidebar-item">Admin Account</div>
+        <div>Admin Account</div>
         <ul className="sidebar-area">
           <li
             className="sidebar-item"

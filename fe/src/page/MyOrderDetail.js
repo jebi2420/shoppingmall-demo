29b81d5d8c6e6a4c20ca3e7b3d52e1b0e1992774
jebi2420 <jebi2420@gmail.com>
2024-06-13 17:37:04 +0900

    [chore] 상품 상세정보 보여주기

--- fe/src/page/MyOrderDetail.js
+++ fe/src/page/MyOrderDetail.js
@@ -7,6 +7,7 @@ import { orderActions } from "../action/orderAction";
 import { SortByLatest } from "../utils/number";
 import  LoadingSpinner  from "../component/LoadingSpinner";
 import { formatTimestamp } from '../utils/number';
+import { currencyFormat } from "../utils/number";
 import "../style/myOrderDetail.style.css";
 
 const MyOrderDetail = () => {
@@ -15,7 +16,7 @@ const MyOrderDetail = () => {
   const { orderList } = useSelector((state) => state.order);
   const { loading } = useSelector((state) => state.order);
   const { selectedOrder } = useSelector((state) => state.order);
-  const item = selectedOrder?.data;
+  const orderItem = selectedOrder?.data;
   const {id} = useParams();
   const statusComponents = {
     preparing: <div className='order-status preparing'>상품준비중</div>,
@@ -27,7 +28,7 @@ const MyOrderDetail = () => {
   
   //오더리스트 들고오기
   useEffect(()=>{
-    console.log("item", item)
+    console.log("item", orderItem)
     dispatch(orderActions.getOrderDetail(id));
   },[dispatch]);
 
@@ -40,32 +41,75 @@ const MyOrderDetail = () => {
     <Container>
       <Row>
         <Col>
-          <div className="myorder-detail-container">
-            <section className="my-order">
-              <h3>주문상세내역</h3>
-              <ul>
-                <li>주문일자 {formatTimestamp(item.createdAt)}</li>
-                <li>주문번호 {item.orderNum}</li>
-              </ul>
-              {renderStatusComponent(item.status)}
-            </section>
-            
-            <section className="my-detail">
-              <h3>주문상품정보</h3>
-            </section>
+          {orderItem.items.length > 0 ? (
+            <div className="myorder-detail-container">
+              <section className="my-order">
+                <h3>주문상세내역</h3>
+                <ul>
+                  <li>주문일자 {formatTimestamp(orderItem.createdAt)}</li>
+                  <li>주문번호 {orderItem.orderNum}</li>
+                </ul>
+                {renderStatusComponent(orderItem.status)}
+              </section>
+              
+              <section className="my-product">
+                <h3>주문상품정보</h3>
+                <ul className='my-prodcut-item-list'>               
+                    {orderItem.items.map((item)=>(
+                        <li onClick={() => navigate(`/product/${item.productId._id}`)}>
+                          <img
+                            src={item.productId.image}
+                            alt="orderImage"
+                            height={96}
+                          />
+                          <span>{item.productId.name}</span>
+                          <span>₩ {currencyFormat(item.price)}</span>
+                          <span>수량 {item.qty} 개</span>
+                        </li>
+                    ))
+                    }
+                </ul>
+              </section>
 
-            <section className="my-buyer">
-              <h3>구매자정보</h3>
-            </section>
+              <section className="my-buyer">
+                <h3>구매자정보</h3>
+                <div>
+                  <div>
+                    <div>주문자</div>
+                    <div>{orderItem.userId.name}</div>
+                  </div>
+                  <div>
+                    <div>이메일</div>
+                    <div>{orderItem.userId.email}</div>
+                  </div>
+                </div>
+              </section>
 
-            <section className='my-payment'>
-              <h3>결제정보</h3>
-            </section>
+              <section className='my-payment'>
+                <h3>결제정보</h3>
+                <div>
+                  <div>결제금액</div>
+                  <h5>₩ {currencyFormat(orderItem.totalPrice)}</h5>
+                </div>
+              </section>
 
-            <section className='my-address'>
-              <h3>배송지정보</h3>
-            </section>
-          </div>
+              <section className='my-address'>
+                <h3>배송지정보</h3>
+                <div>
+                  <div>받는사람</div>
+                  <div>{orderItem.contact.lastName}{orderItem.contact.firstName}</div>
+                </div>
+                <div>
+                  <div>전화번호</div>
+                  <div>{orderItem.contact.contact}</div>
+                </div>
+                <div>
+                  <div>주소</div>
+                  <div>{orderItem.shipTo.address}</div>
+                </div>
+              </section>
+            </div>
+          ):("")}
         </Col>
       </Row>
     </Container>
